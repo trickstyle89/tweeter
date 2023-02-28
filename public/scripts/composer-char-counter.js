d6ed59af54621c82491647923d20a68b5a59fc10
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-28 14:24:51 -0500

    About to test jQuery code.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,12 @@
 console.log('Testing time');
 
 $(document).ready(function() {
-    // --- our code goes here ---
+  // Select the textarea element inside the form in the .new-tweet section
+  let textarea = $('.new-tweet form textarea');
+    
+  // Register an event handler for the input event on the textarea
+  textarea.on('input', function() {
+    // Code to be executed when the input event is triggered
+    console.log('Textarea input event triggered!');
   });
-  
+});
