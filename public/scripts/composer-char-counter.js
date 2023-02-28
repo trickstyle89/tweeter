e85fed59353400d719dfb0fea2f0244b56a17e61
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-28 14:52:59 -0500

    Getting the input to register and the remaining numbers to count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,25 +1,24 @@
 $(document).ready(function() {
+  // Select the textarea element inside the form in the .new-tweet section
+  let textarea = $('.new-tweet form textarea');
+  console.log('text area line 5', textarea);
 
-  // Selects the textarea element inside the form in the .new-tweet section
-  let textarea = $('.net-tweet form textarea');
-
-  //Select the counter element
+  // Select the counter element
   let counter = textarea.siblings('.counter');
   console.log('counter line 8', counter);
 
-
-  //Register an event handler for the input event on the textarea
+  // Register an event handler for the input event on the textarea
   textarea.on('input', function() {
-
     // Get the length of the input value of the textarea
     let length = $(this).val().length;
     console.log('Input length line 14', length);
 
-    // Counts the remaing number of characters
+    // Calculate the remaining characters
     let remaining = 140 - length;
+    console.log('remaining characters line, 21', remaining);
 
-    //update the counter on the page.
+    // Update the counter on the page
     counter.text(remaining);
-
   });
 });
+  
