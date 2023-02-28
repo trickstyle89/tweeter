fc9611a32c233c1f437b1551518677926357eb3d
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-28 14:45:01 -0500

    Not working Debugging attempt no 1

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
-  // Select the textarea element inside the form in the .new-tweet section
-  let textarea = $('.new-tweet form textarea');
-    
-  // Register an event handler for the input event on the textarea
+
+  // Selects the textarea element inside the form in the .new-tweet section
+  let textarea = $('.net-tweet form textarea');
+
+  //Select the counter element
+  let counter = textarea.siblings('.counter');
+  console.log('counter line 8', counter);
+
+
+  //Register an event handler for the input event on the textarea
   textarea.on('input', function() {
-    // Code to be executed when the input event is triggered
-    console.log('Textarea input event triggered!');
+
+    // Get the length of the input value of the textarea
+    let length = $(this).val().length;
+    console.log('Input length line 14', length);
+
+    // Counts the remaing number of characters
+    let remaining = 140 - length;
+
+    //update the counter on the page.
+    counter.text(remaining);
+
   });
 });
