5c7d3afcaeeaa288ae624cace377ee62ef626e85
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-03-01 15:11:56 -0500

    Trying to implement renderTweets to loop over Object Array

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,27 +4,40 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// eslint-disable-next-line no-unused-vars
-const tweetsObj =  {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+
+const tweetsObj = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
 $(document).ready(function() {
 
-const createTweetElement = function(tweetsObj) {
-const { name, avatars, handle } = tweetsObj.user;
-const { text } = tweetsObj.content;
-const dateCreated = tweetsObj.created_at;
+  const createTweetElement = function(tweetsObj) {
+    const { name, avatars, handle } = tweetsObj.user;
+    const { text } = tweetsObj.content;
+    const dateCreated = tweetsObj.created_at;
 
-const tweetHtml = `
+    const tweetHtml = `
     <article class="tweet">
       <header>
         <img src="${avatars}" alt="User avatar">
@@ -45,23 +58,31 @@ const tweetHtml = `
     </article>
   `;
 
-  // Return the tweet jQuery object
-  return tweetHtml;
-};
-
-const renderTweets = function() {
-const $tweet = createTweetElement(tweetsObj);
-console.log('tweets', $tweet);
-$("#tweets-container").append($tweet);
-console.log($("#tweets-container"));
-};
-
-renderTweets();
+    // Return the tweet jQuery object
+    return $(tweetHtml);
+  };
 
 
+  const renderTweets = function(tweets) {
+    // Get the tweets container element
+    const $tweetsContainer = $('#tweets-container');
+      
+    // Loop through the tweets array
+    for (const tweet of tweets) {
+      // Create a tweet element with the tweet data
+      const $tweet = createTweetElement(tweet);
+      
+      // Append the tweet element to the tweets container
+      $tweetsContainer.append($tweet);
+    }
+  };
+
+  renderTweets();
 
 
-/*
+
+
+  /*
 
 //example from class
 const createPostElement = function() {
@@ -72,7 +93,7 @@ const createPostElement = function() {
 
 const arrayOfTitles = ["Item", "name", "Date"]
 
-/* 
+/*
 
 $(document).ready(() => {
     for (const title of arrayOfTitles) {
