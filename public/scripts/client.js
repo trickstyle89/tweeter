e002d7474d9d8f8e63596b7643282686fb942727
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-03-01 09:53:39 -0500

    Client JS. Starting to use data object

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,19 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// eslint-disable-next-line no-unused-vars
+const tweets =  {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+};
+
+const createTweetElement = function() {}
+
+const $tweet = $(`<article class="tweet">Hello world</article>`);
