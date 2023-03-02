a6c2a1dac9c48daf3b09d4c77acd6264a07cc9b9
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-03-02 10:39:40 -0500

    Attemting prepend but need to stepback to troubleshoot.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,11 +5,25 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
+// I strongly believe that I am getting this error because I am only sending the text as empty and it is 
+// *** How is this still working even with this error?
+// Unchecked runtime.lastError: The message port closed before a response was received.
+// Does the fact that I have nested my entire code in the .ready(function) a reason for my issues?
+// If so what can I take out and keep in?
+//
+
+
+
+$(document).ready(function() {  // my whole function is wrapped in this.  A good or bad thing?
 
   const createTweetElement = function(tweetData) {
-    const { name, avatars, handle } = tweetData.user;
-    const { text } = tweetData.content;
+    // const { name, avatars, handle } = tweetData.user; // object destructuring is not working because new tweets don't have this.
+
+    const name = tweetData.user;   // ? tweetData.user.name : 'Ivan Chew';
+    const avatars = tweetData.user;  //  ? tweetData.user.avatars : '/images/profile-hex.png';  ** attempt at conditionals to help offset the error.
+    const handle = tweetData.user;  // ? tweetData.user.handle : '@ChewStory'; 
+    
+    const { text } = tweetData.content; // ? tweetData.content: dataForm; // even with the use of ternary operators. I still get errors once it reachs down here.
     const dateCreated = timeago.format(tweetData.created_at);
 
     const tweetHtml = `
@@ -23,7 +37,7 @@ $(document).ready(function() {
         ${text}
       </div>
       <footer>
-        <span class="tweet-age">${(dateCreated)}</span>
+        <span class="tweet-age">${dateCreated}</span>
         <div class="actions">
           <i class="fa fa-flag"></i>
           <i class="fa fa-retweet"></i>
@@ -37,17 +51,27 @@ $(document).ready(function() {
     return $(tweetHtml);
   };
 
-  const renderTweets = function(tweets) {
+  const renderTweets = function(tweets) { // prepend = false
     // Get the tweets container element
     const $tweetsContainer = $('#tweets-container');
-      
+    
+    tweets.reverse(); // reverses the order?
+
     // Loop through the tweets array
     for (const tweet of tweets) {
       // Create a tweet element with the tweet data
       const $tweet = createTweetElement(tweet);
       
       // Append the tweet element to the tweets container
+     
       $tweetsContainer.append($tweet);
+
+      /*  if (prepend) {
+        $tweetsContainer.prepend($tweet);  // *** trying to prepend
+      } else {
+        $tweetsContainer.append($tweet);
+      }
+      */
     }
   };
 
@@ -89,8 +113,9 @@ $(document).ready(function() {
       return;
     }
     
-    // Serialize the form data
+    // Serialize the form data into a text string.
     const formData = $(this).serialize();
+    console.log('line 100 is coming out as serialized data', formData);
     
     // Use AJAX to submit a POST request that sends the serialized data to the server
     $.ajax({
@@ -102,8 +127,9 @@ $(document).ready(function() {
       .then(function(response) {
 
         // If the request is successful, render the new tweet
+        // renderTweets([response], true);  // trying to use for append.
         renderTweets([response]);
-      
+
         // Clear the tweet input
         $('#tweet-text').val('');
       
